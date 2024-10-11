555a560418a6303be8424dd95271e99998c4fdbd
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2024-10-12 01:27:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,118 @@
+
+import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
+import { gsap } from 'https://cdn.skypack.dev/gsap';
+
+const camera = new THREE.PerspectiveCamera(
+    10,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+camera.position.z = 13;
+
+const scene = new THREE.Scene();
+let robot;
+let mixer;
+const loader = new GLTFLoader();
+loader.load('/mech_drone.glb',
+    function (gltf) {
+        robot = gltf.scene;
+        scene.add(robot);
+        robot.position.x=0.5
+        robot.position.z=6
+        robot.position.y=-0.3
+
+        robot.rotation.y=2.9
+        robot.rotation.x=0
+        robot.rotation.z=-0.1
+
+        mixer = new THREE.AnimationMixer(robot);
+        mixer.clipAction(gltf.animations[0]).play();
+        modelMove();
+    },
+    function (xhr) {},
+    function (error) {}
+);
+const renderer = new THREE.WebGLRenderer({alpha: true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById('container3D').appendChild(renderer.domElement);
+
+// light
+const ambientLight = new THREE.AmbientLight(0xffffff,1.3);
+scene.add(ambientLight);
+
+const topLight = new THREE.DirectionalLight(0xffffff, 1);
+topLight.position.set(500,500,500);
+scene.add(topLight);
+
+
+const reRender3D = () => {
+    requestAnimationFrame(reRender3D);
+    renderer.render(scene, camera);
+    if(mixer) mixer.update(0.02);
+};
+reRender3D();
+
+let arrPositionModel = [
+    {
+        id: 'banner',
+        position: {x: 0.85, y: -0.15, z: 7},
+        rotation: {x: 0, y: 2.9, z: -0.1}
+    },
+    {
+        id: "intro",
+        position: { x: -0.5, y: -0.15, z: 6 },
+        rotation: { x: 0, y: 4.5, z: -0.1 },
+    },
+    {
+        id: "description",
+        position: { x: 0.5, y: -0.3, z:7  },
+        rotation: { x: 0, y: 2.9, z: -0.1 },
+    },
+    {
+        id: "contact",
+        position: { x: 0.85, y: -0.15, z: 7 },
+        rotation: { x: 0, y: 2, z: -0.1 },
+    },
+];
+const modelMove = () => {
+    const sections = document.querySelectorAll('.section');
+    let currentSection;
+    sections.forEach((section) => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top <= window.innerHeight / 3) {
+            currentSection = section.id;
+        }
+    });
+    let position_active = arrPositionModel.findIndex(
+        (val) => val.id == currentSection
+    );
+    if (position_active >= 0) {
+        let new_coordinates = arrPositionModel[position_active];
+        gsap.to(robot.position, {
+            x: new_coordinates.position.x,
+            y: new_coordinates.position.y,
+            z: new_coordinates.position.z,
+            duration: 3,
+            ease: "power1.out"
+        });
+        gsap.to(robot.rotation, {
+            x: new_coordinates.rotation.x,
+            y: new_coordinates.rotation.y,
+            z: new_coordinates.rotation.z,
+            duration: 3,
+            ease: "power1.out"
+        })
+    }
+}
+window.addEventListener('scroll', () => {
+    if (robot) {
+        modelMove();
+    }
+})
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+})
